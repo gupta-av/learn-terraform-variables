dd3691b3b9b18fb93e7915700d12df49ffd385e0
TEK Student <TEKstudent@vlab.lan>
2025-10-08 08:41:54 -0700

    Remove cloud block

--- modules/aws-instance/main.tf
+++ modules/aws-instance/main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "app" {
     sudo yum install httpd -y
     sudo systemctl enable httpd
     sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    echo "<html><body><div>Hello, Avinash!</div></body></html>" > /var/www/html/index.html
     EOF
 
   tags = var.tags
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_dns_name" {
   description = "Public DNS names of the load balancer for this project"
   value       = module.elb_http.this_elb_dns_name
 }
+
+output "ec2_intances" {
+  description = "EC2 instances for this project"
+  value       = module.ec2_instances.instance_ids
+}
